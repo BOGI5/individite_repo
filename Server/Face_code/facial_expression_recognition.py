a4648b8f41c18fbdf975b9b7aa09e9ae1a348c39
stoyanov787 <98825069+stoyanov787@users.noreply.github.com>
2023-03-09 11:53:51 +0200

    Update facial_expression_recognition.py

--- Server/Face_code/facial_expression_recognition.py
+++ Server/Face_code/facial_expression_recognition.py
@@ -9,12 +9,13 @@ IMAGE = os.listdir(DIRECTORY)[0]
 PATH_IMG = os.path.join(DIRECTORY, IMAGE)
 
 
-def expression():
+def face_features():
     """Function which identify facial emotion.
-    :result(str): Facial emotion
+    :emotion(str): Facial emotion
+    :gender(str): Denger
     :img(numpy.ndarray): NumPy tensor representation of the image
-    :return: Facial emotion
-    :rtype: str
+    :return: Facial emotion and Gender
+    :rtype: str, str
     """
     # Convert the image to NumPy array with data type unsigned int(8 bit)
     img = tf.keras.preprocessing.image.load_img(PATH_IMG)
@@ -22,9 +23,12 @@ def expression():
 
     # Predict whether facial emotion is
     # anger, fear, neutral, sad, disgust, happy or surprise
-    result = analyze(img, actions=['emotion'], enforce_detection=False)
-    result = result[0]['dominant_emotion']
+    # Predict gender
+    features = analyze(img, actions=['emotion', 'gender'], enforce_detection=False)
+    emotion = features[0]['dominant_emotion']
+    gender = features[0]['dominant_gender']
 
+    # Delete the images
     os.remove(PATH_IMG)
 
-    return result
+    return emotion, gender
