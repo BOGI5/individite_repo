cd22c420fdb789a7df3837f749d6742f3e3d8710
CooDDiiNNgg <niki@valkanovi.com>
2023-03-10 16:27:47 +0200

    added_generate_password_to_folder

--- Server/Password_work/__init__.py
+++ Server/Password_work/__init__.py
@@ -2,6 +2,7 @@ from . import password_encrypting
 from . import password_decrypting
 from . import hashing
 from . import Test_password
+from . import password_generator
 
 def encrypt(user, password, website, key, password_verify, username):
     return password_encrypting.encrypt_password(user, password, website, key, password_verify, username)
@@ -22,3 +23,6 @@ def common_used_passwords(user, website, key, username, password_verify):
     if decrypt(user, website, key, username, password_verify):
         return Test_password.common_used_passwords(str(decrypt(user, website, key, username, password_verify)))
     return False
+
+def generate_password(len, key_word=""):
+    return password_generator.generate_password(len, key_word)
