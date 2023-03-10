0bd8bd2ebaa436c98c81bbd2deb3889c1a6f0014
BOGI5 <Bogdanyakov5@gmail.com>
2023-03-10 17:44:32 +0200

    Shit removed

--- Server/Actual_server/4TA_PAPKA/app.py
+++ Server/Actual_server/4TA_PAPKA/app.py
@@ -5,29 +5,24 @@ from base64_to_image import get_image
 
 app = Flask(__name__)
 
-app.config['UPLOAD_FOLDER'] = r'/media/manol/ESD-USB/hacktues/hacktues/uploads'
+#app.config['UPLOAD_FOLDER'] = r'/media/manol/ESD-USB/hacktues/hacktues/uploads'
 
 
-@app.route('/signup/', methods=['GET', 'POST'])
+
+
+@app.route('/signup/', methods =["GET", "POST"])
 def signup():
     if request.method == "POST":
         takendataURL = request.form.get("dataURL")
         username = request.form.get("Name")
+
+        print(takendataURL)
+        print(username)
         get_image(takendataURL, username)
 
     return render_template('signup.html')
 
-@app.route('/passwords/')
-def passwords():
-    #if request.method == "POST":
-       # newwebsite = request.form.get("site_url")
-       # newpass = request.form.get("site_pass")
-        #print(newwebsite + newpass)
-    return render_template('passwords.html')
 
-@app.route('/login')
-def login():
-    return render_template('login.html')
 
 
 if __name__ == '__main__':
