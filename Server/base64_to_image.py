9ba117ddb4d206bee819c1692d1ca41d8e67a374
stoyanov787 <98825069+stoyanov787@users.noreply.github.com>
2023-03-10 10:17:25 +0200

    Update base64_to_image.py

--- Server/base64_to_image.py
+++ Server/base64_to_image.py
@@ -4,12 +4,21 @@ import base64
 
 def get_image():
     """Function which decode base64 data and create image.
+    :name(str): Name of the user
     :data(str): Two halves of the base64 data
     :text(str): Base64 data
     :img(str): Image type i.e. jpeg, jpg, png etc.
     """
+    # Get name and image files path
+    name_path = r'name.txt'
+    img_path = r'image.txt'
+
+    # Extract name
+    with open(name_path, 'r') as file:
+        name = file.read()
+
     # Extract base64 data
-    with open('message.txt', 'r') as file:
+    with open(img_path, 'r') as file:
         text = file.read()
 
     # Remove the type from the base64 data
@@ -24,5 +33,6 @@ def get_image():
     text = base64.decodebytes(text)
 
     # Crete image
-    with open(rf'image.{image}', 'wb') as file:
+    new_img = rf'Face_images/{name}.{image}'
+    with open(new_img, 'wb') as file:
         file.write(text)
