18b35f0aa6a21910a2607213b7b6ca778fcde15f
BOGI5 <Bogdanyakov5@gmail.com>
2023-03-10 14:40:41 +0200

    website update

--- Server/Actual_server/static/form.js
+++ Server/Actual_server/static/form.js
@@ -63,6 +63,8 @@ function fullReset(){
     document.getElementById('Password').value = "";
     document.getElementById('takePhoto').style.display="block";
     document.getElementById('submit').style.display="none";
+    document.getElementById('Name').readOnly = false;
+        document.getElementById('Password').readOnly = false;
 }
 
 function Form(){
