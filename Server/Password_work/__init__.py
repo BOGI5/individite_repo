038a96f5490128ebdc728af9f8b587ad1c244dcb
CooDDiiNNgg <niki@valkanovi.com>
2023-03-10 23:35:11 +0200

    test

--- Server/Password_work/__init__.py
+++ Server/Password_work/__init__.py
@@ -2,7 +2,7 @@ from . import password_encrypting
 from . import password_decrypting
 from . import hashing
 from . import Test_password
-from . import password_generator
+# from . import password_generator
 
 
 def encrypt(user, password, website, key, password_verify, username):
@@ -25,5 +25,5 @@ def common_used_passwords(user, website, key, username, password_verify):
         return Test_password.common_used_passwords(str(decrypt(user, website, key, username, password_verify)))
     return False
 
-def generate_password(len, key_word=""):
-    return password_generator.generate_password(len, key_word)
+# def generate_password(len, key_word=""):
+#     return password_generator.generate_password(len, key_word)
