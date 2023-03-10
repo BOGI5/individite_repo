7eae1b643efde04e848649a8b17b7fd6c776488c
stoyanov787 <98825069+stoyanov787@users.noreply.github.com>
2023-03-10 12:28:14 +0200

    Update facial_expression_recognition.py

--- Server/Face_code/facial_expression_recognition.py
+++ Server/Face_code/facial_expression_recognition.py
@@ -1,34 +1,31 @@
 """Facial expression recognition"""
 import os
-import tensorflow as tf
+from tensorflow import keras
 from deepface.DeepFace import analyze
-
-# Get image's path
-DIRECTORY = r'Face_expressions'
-IMAGE = os.listdir(DIRECTORY)[0]
-PATH_IMG = os.path.join(DIRECTORY, IMAGE)
 
 
 def face_features():
     """Function which identify facial emotion.
     :emotion(str): Facial emotion
-    :gender(str): Denger
     :img(numpy.ndarray): NumPy tensor representation of the image
     :return: Facial emotion and Gender
-    :rtype: str, str
+    :rtype: str
     """
+    # Get image's path
+    directory = r'Face_expressions'
+    image = os.listdir(directory)[0]
+    path_image = os.path.join(directory, image)
+
     # Convert the image to NumPy array with data type unsigned int(8 bit)
-    img = tf.keras.preprocessing.image.load_img(PATH_IMG)
-    img = tf.keras.preprocessing.image.img_to_array(img, dtype='uint8')
+    img = keras.preprocessing.image.load_img(path_image)
+    img = keras.preprocessing.image.img_to_array(img, dtype='uint8')
 
     # Predict whether facial emotion is
     # anger, fear, neutral, sad, disgust, happy or surprise
-    # Predict gender
-    features = analyze(img, actions=['emotion', 'gender'], enforce_detection=False)
+    features = analyze(img, actions=['emotion'], enforce_detection=False)
     emotion = features[0]['dominant_emotion']
-    gender = features[0]['dominant_gender']
 
     # Delete the images
-    os.remove(PATH_IMG)
+    os.remove(path_image)
 
-    return emotion, gender
+    return emotion
