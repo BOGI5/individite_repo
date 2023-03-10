df3578584e2ae62290affd5714bc0513f9bcf461
CooDDiiNNgg <niki@valkanovi.com>
2023-03-10 13:15:24 +0200

    Better_database

--- Server/Password_work/password_encrypting.py
+++ Server/Password_work/password_encrypting.py
@@ -11,33 +11,31 @@ def get_key(key):
 def save_to_file(user, password, website, username):
     # get the path to the file
     path = os.path.dirname(os.path.abspath(__file__))
-    path += r"/database.json"
+    path += r"/Database/new_database/" + str(username) + ".json"
+    if not os.path.exists(path):
+        open(path, "w").close()
     file = open(path, "r")
     # checks if file is empty
     if os.path.getsize(path) > 0:
     # if json_object:
         data = file.read()
         data = json.loads(data)
-        if (username in data):
-            # checks if user is in file
-            if (user in data[username]):
-                # checks if website is in file
-                if(website in data[username][user]):
-                    del data[username][user][website]["password"]
-                    new_data = {"password": str(password)}
-                    data[username][user][website].update(new_data)
-                else:
-                    new_data = {str(website):{"password": str(password)}}
-                    data[username][user].update(new_data)
+        # checks if user is in file
+        if (user in data):
+            # checks if website is in file
+            if(website in data[user]):
+                del data[user][website]["password"]
+                new_data = {"password": str(password)}
+                data[user][website].update(new_data)
             else:
-                new_data = {str(user): {str(website):{ "password": str(password)}}}
-                data[username].update(new_data)
+                new_data = {str(website):{"password": str(password)}}
+                data[user].update(new_data)
         else:
-            new_data = {str(username):{str(user): {str(website):{ "password": str(password)}}}}
+            new_data = {str(user): {str(website):{ "password": str(password)}}}
             data.update(new_data)
             
     else:
-        data = {str(username):{str(user): {str(website):{ "password": str(password)}}}}
+        data = {str(user): {str(website):{ "password": str(password)}}}
 
     file.close()
     file = open(path, "w")
@@ -54,7 +52,7 @@ def encrypt_password(user, password, website, key, password_verify, username):
     user = user.encode().decode()
     website = website.encode().decode()
     password = Fernet(key).encrypt(password)
-    return save_to_file(user, password.decode(), website, username)
+    save_to_file(user, password.decode(), website, username)
 
 
 
--- Server/testing.py
+++ Server/testing.py
@@ -5,7 +5,7 @@ str_img = Face_code.face_verification.check_image()
 
 if str_img:
     # tell what password on what user to save and encrypt
-    Password_work.encrypt("sdf", "password", "te", str_img, "parola", "user")
-    print(Password_work.decrypt("sdf", "te", str_img, "user", "parloa"))
-    print(Password_work.check_password("sdf", "te", str_img, "user", "parloa"))
-    print(Password_work.common_used_passwords("sdf", "te", str_img, "user", "parloa"))
+    Password_work.encrypt("sdf", "password", "te", str_img, "parola", "user_2")
+    print(Password_work.decrypt("sdf", "te", str_img, "user_2", "parola"))
+    print(Password_work.check_password("sdf", "te", str_img, "user_2", "parola"))
+    print(Password_work.common_used_passwords("sdf", "te", str_img, "user_2", "parola"))
