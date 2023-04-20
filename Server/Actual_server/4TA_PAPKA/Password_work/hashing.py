8bf764d7bfce097e560d0f71eb59ca985134f466
atanas-roujinov <atahacr@gmail.com>
2023-04-20 13:22:08 +0300

    no more 4ta 4TA_PAPKA

--- Server/Actual_server/4TA_PAPKA/Password_work/hashing.py
+++
@@ -1,8 +0,0 @@
-import hashlib
-import base64
-
-def hash_key(key):
-    key = hashlib.sha256(key.encode()).hexdigest()
-    key = bytes.fromhex(key)
-    key = base64.b64encode(key).decode()
-    return key
